4399e23b6a67cca3d298fb5b79552040447c7667
lily-akopyan <lily.akopyan@gmail.com>
2023-05-03 13:45:54 +0200

    Add playRound fun
    Function plays round of rock paper scissors and returns winner

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ function getComputerChoice() {
 function getUserChoice() {
     return prompt("Rock, Paper or Scissors? ");;
 }
+
+function playRound(computerSelection, userSelection) {
+    computerSelection = getComputerChoice();
+    userSelection = getUserChoice().trim().toLowerCase();
+    let outcome;
+    if (computerSelection == userSelection) {
+        outcome = "Draw";
+    } else if (computerSelection == "rock") {
+        if (userSelection == "paper") {
+            outcome = "You win! Paper beats Rock!";
+        } else {
+            outcome = "You lose! Rock beats Scissors!";
+        }
+    } else if (computerSelection == "paper") {
+        if (userSelection == "rock") {
+            outcome = "You lose! Paper beats Rock!";
+        } else {
+            outcome = "You win! Scissors beat Paper!";
+        }
+    } else {
+        if (userSelection == "rock") {
+            outcome = "You win! Rock beats Scissors!";
+        } else {
+            outcome = "You lose! Scissors beat Paper!";
+        }
+    }
+    console.log("Computer chose: " + computerSelection);
+    console.log("You chose: " + userSelection);
+    return outcome;
+}
