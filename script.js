b7d840a6ce7edcac509a57a20c2b8dd24d73a797
lily-akopyan <lily.akopyan@gmail.com>
2023-07-17 13:30:05 +0200

    comments

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
+//Event listeners for the user choice buttons 
+//and set the user choice based on click
 const btns = document.querySelectorAll('.choicebtn');
 console.log(btns);
 btns.forEach(btn => {
     btn.addEventListener('click', getUserChoice);
 })
 
-function getComputerChoice() {
-    const output = ["rock", "paper", "scissors"];
-    let ran = Math.floor(Math.random() * 3);
-    return output[ran];
-}
-
-let computerScore = 0;
-let userScore = 0;
-let outcome;
-
+//Read id of clicked button and set userChoice to that id
 function getUserChoice(e) {
     let userChoice = (e.target.id);
     console.log(userChoice);
     playRound(getComputerChoice(), userChoice)
 }
 
+//Randomise computer choice
+function getComputerChoice() {
+    const output = ["rock", "paper", "scissors"];
+    let ran = Math.floor(Math.random() * 3);
+    return output[ran];
+}
 
+//Initialize variables
+let computerScore = 0;
+let userScore = 0;
+let outcome;
+
+//Play one round and display results
 function playRound(computerSelection, userSelection) {
     if (computerSelection == userSelection) {
         outcome = "Draw";
@@ -50,9 +55,13 @@ function playRound(computerSelection, userSelection) {
     return outcome;
 }
 
+//Capitalise first letter of a string 
 function capitalise(str){
 return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+
+//Old play game function for console no user output 
 
 /*function game() {
     for (let i = 0; i < 5; i++) {
